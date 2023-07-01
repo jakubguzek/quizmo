7a173e64d5afa4321f8480f616eb12341f40aca2
jakubguzek <jakub.j.guzek@gmail.com>
2023-07-01 22:31:56 +0200

    use questions from questions.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const questions = [
-  {
-    id: 0,
-    question: "Dlaczego TMV jest dobrym modelem badawczym?",
-    answers: [
-      { id: 0, text: "Jest przonoszony przez owady, nicienie, grzyby" },
-      { id: 1, text: "Jest przenoszony poprzez pocieranie (mechaniczne uszkodzenie)" },
-      { id: 2, text: "Infekcja TMV wywołuje łatwo rozpoznawalne symptomy" },
-      { id: 3, text: "Wszystkie odpowiedzi są prawdziwe" }
-    ],
-    correct: "Infekcja TMV wywołuje łatwo rozpoznawalne symptomy"
-  }
-];
 var currentQuestion = {}
 const questionElement = document.getElementById("question-text");
 const formElement = document.getElementById("answers-form");
